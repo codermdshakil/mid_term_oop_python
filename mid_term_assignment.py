817f9ddea57050428d0c19eacecbf9177bb15a57
codermdshakil <ahmedshakil0512@gmail.com>
2024-12-13 00:14:54 +0600

    Added : In book object borrow_book, retrun_book methods in book object

--- mid_term_assignment.py
+++ mid_term_assignment.py
@@ -3,15 +3,42 @@ class Library:
     
     book_list = []
     
-    def entry_book(book):
-        Library.book_list.append(book)
+    
         
         
 class Book:
     
     def __init__(self, book_id, title, author, avaiablability):
-        self.book_id = book_id
-        self.title = title
-        self.author = author
-        self.avaiablability = avaiablability
+        self.__book_id = book_id
+        self._title = title
+        self._author = author
+        self._avaiablability = avaiablability
+    
+    def entry_book(self):
+        Library.book_list.append(self)
+        
+    def borrow_book(self):
+        if self._avaiablability == True:
+            self._avaiablability = False
+        else:
+            self._avaiablability == True
+            
+    def return_book(self):
+        if self._avaiablability == False:
+            self._avaiablability = True
+    
+    def view_book_info(self):
+        return f"Book id is : {self.__book_id}, Book name is : {self._title}, Book Author: {self._author} and Avaiablability is {self._avaiablability}"
+    
+
+
+python_book = Book(1,'Head first Python', 'Shakil', True)
+
+
+print(python_book.view_book_info())
+
+
+
+
+        
         
